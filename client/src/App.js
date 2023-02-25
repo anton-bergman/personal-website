b9fa897a76f8680833ea6359ad6fdcb326308284
Anton Bergman <antonbergman@MBPsomtorAnton2.bredband2.com>
2023-02-25 09:42:36 +0100

    troubleshooting

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,19 @@ import Footer from './components/Footer';
 
 function App() {
 
+  try {
+    const response = await fetch(BACKEND_URL + "/*", {
+        method: "GET",
+        headers: {"Content-Type": "text/html"},
+        body: {}
+    });
+    if (response.ok) {
+        alert("Email sent!");
+    }
+} catch (error) {
+    console.log(error);
+}
+
   const [backendData, setBackendData] = useState([{}])
 
   useEffect(() => {
