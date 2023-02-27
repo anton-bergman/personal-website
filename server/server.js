370fa4c3e707b98f1eaeb4c87bb269a278d93dc8
Anton Bergman <antonbergman@LUXOR-TV.localdomain>
2023-02-27 11:57:14 +0100

    bug fixes with CORS

--- server/server.js
+++ server/server.js
@@ -22,10 +22,7 @@ app.use(express.static(path.join(__dirname, '../client/build')));
 // Setup CORS (Cross-Origin Resource Sharing)
 const allowedOrigins = [process.env.FRONTEND_URL, process.env.FRONTEND_URL_WWW, process.env.FRONTEND_URL_TEST];
 app.use((req, res, next) => {
-    const origin = req.headers.origin;
-    if (allowedOrigins.includes(origin)) {
-      res.setHeader('Access-Control-Allow-Origin', origin);
-    }
+    res.setHeader('Access-Control-Allow-Origin', process.env.FRONTEND_URL_WWW);
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
